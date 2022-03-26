783bca7a261f14a403f3ee88980494907c1be7da
idan10000 <49813824+idan10000@users.noreply.github.com>
2022-03-26 19:12:12 +0300

    basic picture picker

--- pages/post_poster/post_poster_screen.js
+++ pages/post_poster/post_poster_screen.js
@@ -1,13 +1,23 @@
 import * as React from 'react';
 import {Modal, Portal, Text, Button, Provider, Card, Chip} from 'react-native-paper';
-import {View, StyleSheet, } from 'react-native'
+import {View, StyleSheet, TouchableOpacity, Image} from 'react-native'
+import * as ImagePicker from 'expo-image-picker';
 
 
-const MyComponent = () => {
+const PosterPostingComponent = () => {
+
+    //---------------------- Modal ----------------------
     const [visible, setVisible] = React.useState(false);
-
     const showModal = () => setVisible(true);
     const hideModal = () => setVisible(false);
+
+    //---------------------- Tag Selection ----------------------
+    const tagList = [
+        {tag: "Shy", state: false},
+        {tag: "Friendly", state: false},
+        {tag: "Aggressive", state: false}]
+
+    const [tags, toggleState] = React.useState(tagList);
 
     const chipPressHandler = (index) => {
         toggleState(prevStates => {
@@ -17,12 +27,43 @@ const MyComponent = () => {
         });
     }
 
-    const tagList = [
-        {tag: "Shy", state: false},
-        {tag: "Friendly", state: false},
-        {tag: "Aggressive", state: false}]
+    //---------------------- Image Picker ----------------------
+    const [selectedImage, setSelectedImage] = React.useState(null);
+    let openImagePickerAsync = async () => {
+        let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
 
-    const [tags, toggleState] = React.useState(tagList);
+        if (permissionResult.granted === false) {
+            alert('Permission to access camera roll is required!');
+            return;
+        }
+
+        let pickerResult = await ImagePicker.launchImageLibraryAsync();
+
+        if (pickerResult.cancelled === true) {
+            return;
+        }
+
+        setSelectedImage({localUri: pickerResult.uri});
+    };
+
+
+    var imagePicker = <TouchableOpacity style={styles.addImageContainer} onPress={openImagePickerAsync}>
+        <Text style={styles.addImageText}>Pick a picture</Text>
+    </TouchableOpacity>
+
+    if (selectedImage !== null) {
+
+        imagePicker =
+            <View style={styles.pictureContainer}>
+                <Image
+                    source={{  uri: selectedImage.localUri }}
+                    style={styles.card}
+                />
+            </View>
+
+    }
+
+
     return (
         <Provider>
             {/*Modal (pop up screen) for selecting the tags describing the dog*/}
@@ -39,17 +80,18 @@ const MyComponent = () => {
                 </Modal>
             </Portal>
 
-            <Card>
-                <Card.Cover
-                    source={{ uri: 'https://picsum.photos/700' }}/>
-            </Card>
+            <View style={styles.container}>
 
-            <Button style={{marginTop: 30}} onPress={showModal}>
-                Add tags
-            </Button>
-            <Button mode={"contained"}>
-                Submit
-            </Button>
+                {imagePicker}
+
+                <Button style={{marginTop: 30}} onPress={showModal}>
+                    Add tags
+                </Button>
+
+                <Button mode={"contained"} style={{marginBottom: 30}} onPress={openImagePickerAsync}>
+                    Submit
+                </Button>
+            </View>
         </Provider>
     );
 };
@@ -58,7 +100,7 @@ const styles = StyleSheet.create({
     container: {
         paddingTop: 50,
         padding: 4,
-
+        flex: 1
     },
     stretch: {
         width: '100%',
@@ -76,7 +118,29 @@ const styles = StyleSheet.create({
     modal: {
         backgroundColor: 'white',
         padding: 20
-    }
+    },
+    addImageContainer: {
+        justifyContent: "center",
+        borderWidth: 1,
+        flex: 1
+    },
+    addImageText: {
+        textAlign: "center",
+        fontSize: 24
+    },
+    pictureContainer: {
+        marginTop:30,
+        flex:1,
+        justifyContent: "center",
+        alignContent:"center",
+
+    },
+    card: {
+        resizeMode:"contain",
+        flex:1
+
+    },
+
 });
 
-export default MyComponent;
+export default PosterPostingComponent;
