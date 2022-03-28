6777db6274a570a6c2501871082479ed925920a1
idan10000 <49813824+idan10000@users.noreply.github.com>
2022-03-28 16:16:27 +0300

    Basic planning

---
+++ pages/browse_page/browse_page.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Modal, Portal, Text, Button, Provider, Card, Chip} from 'react-native-paper';
+import {View, StyleSheet,} from 'react-native'
+
+
+const BrowsePage = () => {
+    return (
+        <Provider>
+            <View>
+                {/*tab switching */}
+            </View>
+            <View>
+                {/* search bar + sort + filter*/}
+            </View>
+            <View>
+                {/* List */}
+
+            </View>
+        </Provider>
+    )
+
+};
+
+export default BrowsePage;
