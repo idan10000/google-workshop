cc5c9ebf90bbb4f3171131c2d1a067f6e823eb7c
NofarVered <nofarss79@gmail.com>
2022-03-29 16:51:00 +0300

    Nofar

---
+++ screens_nofar/Edit_profile.js
@@ -0,0 +1,191 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ImageBackground,
+  TextInput,
+  StyleSheet,
+} from "react-native";
+
+import { useTheme } from "react-native-paper";
+
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import Feather from "react-native-vector-icons/Feather";
+
+import BottomSheet from "reanimated-bottom-sheet";
+import Animated from "react-native-reanimated";
+
+// import ImagePicker from "react-native-image-crop-picker";
+
+const EditProfileScreen = () => {
+  const { colors } = useTheme();
+  return (
+    <Animated.View
+      style={{
+        margin: 20,
+      }}
+    >
+      <View style={styles.action}>
+        <FontAwesome name="user-o" color={colors.text} size={20} />
+        <TextInput
+          placeholder="שם "
+          placeholderTextColor="#666666"
+          autoCorrect={false}
+          style={[
+            styles.textInput,
+            {
+              color: colors.text,
+            },
+          ]}
+        />
+      </View>
+      <View style={styles.action}>
+        <Feather name="phone" color={colors.text} size={20} />
+        <TextInput
+          placeholder="טלפון"
+          placeholderTextColor="#666666"
+          keyboardType="number-pad"
+          autoCorrect={false}
+          style={[
+            styles.textInput,
+            {
+              color: colors.text,
+            },
+          ]}
+        />
+      </View>
+      <View style={styles.action}>
+        <FontAwesome name="envelope-o" color={colors.text} size={20} />
+        <TextInput
+          placeholder="אימייל"
+          placeholderTextColor="#666666"
+          keyboardType="email-address"
+          autoCorrect={false}
+          style={[
+            styles.textInput,
+            {
+              color: colors.text,
+            },
+          ]}
+        />
+      </View>
+      <View style={styles.action}>
+        <FontAwesome name="globe" color={colors.text} size={20} />
+        <TextInput
+          placeholder="מדינה"
+          placeholderTextColor="#666666"
+          autoCorrect={false}
+          style={[
+            styles.textInput,
+            {
+              color: colors.text,
+            },
+          ]}
+        />
+      </View>
+      <View style={styles.action}>
+        <Icon name="map-marker-outline" color={colors.text} size={20} />
+        <TextInput
+          placeholder="עיר"
+          placeholderTextColor="#666666"
+          autoCorrect={false}
+          style={[
+            styles.textInput,
+            {
+              color: colors.text,
+            },
+          ]}
+        />
+      </View>
+      <TouchableOpacity style={styles.commandButton} onPress={() => {}}>
+        <Text style={styles.panelButtonTitle}>עדכן</Text>
+      </TouchableOpacity>
+    </Animated.View>
+  );
+};
+
+export default EditProfileScreen;
+
+// -----------------------------------------------------------------------------------------
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  commandButton: {
+    padding: 15,
+    borderRadius: 10,
+    backgroundColor: "#FF6347",
+    alignItems: "center",
+    marginTop: 10,
+  },
+  panel: {
+    padding: 20,
+    backgroundColor: "#FFFFFF",
+    paddingTop: 20,
+  },
+  header: {
+    backgroundColor: "#FFFFFF",
+    shadowColor: "#333333",
+    shadowOffset: { width: -1, height: -3 },
+    shadowRadius: 2,
+    shadowOpacity: 0.4,
+    paddingTop: 20,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+  },
+  panelHeader: {
+    alignItems: "center",
+  },
+  panelHandle: {
+    width: 40,
+    height: 8,
+    borderRadius: 4,
+    backgroundColor: "#00000040",
+    marginBottom: 10,
+  },
+  panelTitle: {
+    fontSize: 27,
+    height: 35,
+  },
+  panelSubtitle: {
+    fontSize: 14,
+    color: "gray",
+    height: 30,
+    marginBottom: 10,
+  },
+  panelButton: {
+    padding: 13,
+    borderRadius: 10,
+    backgroundColor: "#FF6347",
+    alignItems: "center",
+    marginVertical: 7,
+  },
+  panelButtonTitle: {
+    fontSize: 17,
+    fontWeight: "bold",
+    color: "white",
+  },
+  action: {
+    flexDirection: "row",
+    marginTop: 10,
+    marginBottom: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#f2f2f2",
+    paddingBottom: 5,
+  },
+  actionError: {
+    flexDirection: "row",
+    marginTop: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#FF0000",
+    paddingBottom: 5,
+  },
+  textInput: {
+    flex: 1,
+    marginTop: 0,
+    paddingLeft: 10,
+    color: "#05375a",
+  },
+});
---
+++ screens_nofar/User_profile.js
@@ -0,0 +1,154 @@
+import React from "react";
+import { View, SafeAreaView, StyleSheet } from "react-native";
+import {
+  Avatar,
+  Title,
+  Caption,
+  Text,
+  TouchableRipple,
+} from "react-native-paper";
+
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+
+const ProfileScreen = () => {
+  const userDetalies = {
+    name: "אמיר כהן",
+    userName: "queenOfEngland18",
+    country: "ישראל",
+    city: "אילת",
+    phone: "052-1111111",
+    email: "abc@gmail.com",
+    image: "https://api.adorable.io/avatars/80/abott@adorable.png",
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.userInfoSection}>
+        <View style={{ flexDirection: "row", marginTop: 15 }}>
+          <Avatar.Image
+            source={{
+              uri: userDetalies.image,
+            }}
+            size={80}
+          />
+          <View style={{ marginRight: 20 }}>
+            <Title
+              style={[
+                styles.title,
+                {
+                  marginTop: 15,
+                  marginBottom: 5,
+                },
+              ]}
+            >
+              {userDetalies.name}
+            </Title>
+            <Caption style={styles.caption}>{userDetalies.userName}</Caption>
+          </View>
+        </View>
+      </View>
+
+      <View style={styles.userInfoSection}>
+        <View style={styles.row}>
+          <Icon name="map-marker-radius" color="#777777" size={20} />
+          <Text style={{ color: "#777777", marginLeft: 20 }}>
+            {userDetalies.city}
+          </Text>
+        </View>
+        <View style={styles.row}>
+          <Icon name="phone" color="#777777" size={20} />
+          <Text style={{ color: "#777777", marginLeft: 20 }}>
+            {userDetalies.phone}
+          </Text>
+        </View>
+        <View style={styles.row}>
+          <Icon name="email" color="#777777" size={20} />
+          <Text style={{ color: "#777777", marginLeft: 20 }}>
+            {userDetalies.email}
+          </Text>
+        </View>
+      </View>
+
+      <View style={styles.menuWrapper}>
+        <TouchableRipple onPress={() => {}}>
+          <View style={styles.menuItem}>
+            <Icon name="heart-outline" color="#FF6347" size={25} />
+            <Text style={styles.menuItemText}>הצג פרסום אישי</Text>
+          </View>
+        </TouchableRipple>
+        <TouchableRipple onPress={() => {}}>
+          <View style={styles.menuItem}>
+            <Icon name="account-edit" color="#FF6347" size={25} />
+            <Text style={styles.menuItemText}>עדכן פרופיל </Text>
+          </View>
+        </TouchableRipple>
+        <TouchableRipple onPress={() => {}}>
+          <View style={styles.menuItem}>
+            <Icon name="share-outline" color="#FF6347" size={25} />
+            <Text style={styles.menuItemText}>שתף אפליקציה</Text>
+          </View>
+        </TouchableRipple>
+        <TouchableRipple onPress={() => {}}>
+          <View style={styles.menuItem}>
+            <Icon name="account-check-outline" color="#FF6347" size={25} />
+            <Text style={styles.menuItemText}>תמיכה טכנית</Text>
+          </View>
+        </TouchableRipple>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default ProfileScreen;
+
+// ----------------------------------------------------------------------------------------
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  userInfoSection: {
+    paddingHorizontal: 30,
+    marginBottom: 25,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  caption: {
+    fontSize: 14,
+    lineHeight: 14,
+    fontWeight: "500",
+  },
+  row: {
+    flexDirection: "row",
+    marginBottom: 10,
+  },
+  infoBoxWrapper: {
+    borderBottomColor: "#dddddd",
+    borderBottomWidth: 1,
+    borderTopColor: "#dddddd",
+    borderTopWidth: 1,
+    flexDirection: "row",
+    height: 100,
+  },
+  infoBox: {
+    width: "50%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  menuWrapper: {
+    marginTop: 10,
+  },
+  menuItem: {
+    flexDirection: "row",
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+  },
+  menuItemText: {
+    color: "#777777",
+    marginRight: 20,
+    fontWeight: "600",
+    fontSize: 16,
+    lineHeight: 26,
+  },
+});
