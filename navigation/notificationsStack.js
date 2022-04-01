7dd840262409490c27892905c05978339ee1d5b3
roeymarinov <roeymarinov@gmail.com>
2022-04-01 12:29:59 +0300

    added map, hide FAB for now

--- navigation/notificationsStack.js
+++ navigation/notificationsStack.js
@@ -5,6 +5,7 @@ import NavigationBar from './navigationBar';
 import { NavigationContainer } from '@react-navigation/native';
 import { FAB } from 'react-native-paper';
 import { StyleSheet } from 'react-native';
+import { useState } from 'react';
 
 
 const Stack = createStackNavigator();
@@ -12,16 +13,19 @@ const Stack = createStackNavigator();
 export default function NotificationsStack() {
   const options = {header: ({navigation}) => <Header navigation={navigation}/>}
 
+  const [inHome, setInHome] = useState(true) 
+  //setInHome(newState.routes[newState.index].name === "בית"
   return (
-    <NavigationContainer>
+    <NavigationContainer onStateChange={(newState) => console.log(newState.routeNames, newState.index)}>
       <Stack.Navigator>
           <Stack.Screen name="Main" component={NavigationBar} options={options}/>
           <Stack.Screen name="התראות" component={NotificationsScreen} />
       </Stack.Navigator>
       <FAB
         style={styles.fab}
-        icon="camera"
+        icon='camera'
         color='white'
+        visible={!inHome}
         onPress={() => console.log('Pressed')}
       />
     </NavigationContainer>
---
+++ screens/map.js
@@ -0,0 +1,13 @@
+import { StyleSheet, View, Image } from 'react-native';
+
+
+
+export default function Map() {
+    return (
+        <View>
+            <Image
+                source={require('../assets/map.png')}
+            />
+        </View>
+    )
+}
