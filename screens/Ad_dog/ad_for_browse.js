3dca37db3c180a789e7bc068fe2a0032f0e39d7e
NofarVered <nofarss79@gmail.com>
2022-04-03 02:29:56 +0300

    nofar- ad\report style

--- screens/Ad_dog/ad_for_browse.js
+++ screens/Ad_dog/ad_for_browse.js
@@ -1,17 +1,38 @@
 import React, { Component } from "react";
-import { Text, View, TouchableOpacity, ScrollView } from "react-native";
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  ScrollView,
+  Linking,
+  Platform,
+} from "react-native";
 import { Nofar_styles } from "../utils/Nofar_style";
 import { AR_styles } from "./Ad_style";
 import AdTemplate from "./ad_template";
+
 export default function AdForBrowse() {
-  const phoneNumber = "0525872822";
+  const makeCall = () => {
+    let phoneNumber = "";
+    if (Platform.OS === "android") {
+      phoneNumber = "tel:${1234567890}";
+    } else {
+      phoneNumber = "telprompt:${1234567890}";
+    }
+    Linking.openURL(phoneNumber);
+  };
+
   return (
     <View style={Nofar_styles.container}>
       <ScrollView style={AR_styles.content}>
         <AdTemplate />
         <View style={{ marginLeft: 15 }}>
           <View style={AR_styles.cardContent}>
-            <TouchableOpacity style={Nofar_styles.BigButton} onPress={() => {}}>
+            <TouchableOpacity
+              style={Nofar_styles.BigButton}
+              onPress={makeCall()}
+              activeOpacity={0.7}
+            >
               <Text style={Nofar_styles.BigButtonText}>צור קשר עם הבעלים</Text>
             </TouchableOpacity>
           </View>
--- screens/Report_dog/report_for_browse.js
+++ screens/Report_dog/report_for_browse.js
@@ -1,17 +1,37 @@
 import React, { Component } from "react";
-import { Text, View, TouchableOpacity, ScrollView } from "react-native";
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  ScrollView,
+  Linking,
+  Platform,
+} from "react-native";
 import { Nofar_styles } from "../utils/Nofar_style";
 import { AR_styles } from "./Report_style";
 
 import ReportTemplate from "./report_template";
 export default function ReportForBrowse() {
+  const makeCall = () => {
+    let phoneNumber = "";
+    if (Platform.OS === "android") {
+      phoneNumber = "tel:${1234567890}";
+    } else {
+      phoneNumber = "telprompt:${1234567890}";
+    }
+    Linking.openURL(phoneNumber);
+  };
+
   return (
     <View style={Nofar_styles.container}>
       <ScrollView style={AR_styles.content}>
         <ReportTemplate />
         <View style={{ marginLeft: 15 }}>
           <View style={AR_styles.cardContent}>
-            <TouchableOpacity style={Nofar_styles.BigButton} onPress={() => {}}>
+            <TouchableOpacity
+              style={Nofar_styles.BigButton}
+              onPress={makeCall()}
+            >
               <Text style={Nofar_styles.BigButtonText}>צור קשר עם המדווח</Text>
             </TouchableOpacity>
           </View>
