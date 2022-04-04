94b3d4078802800a813d39d4aee9a373c43bdaa0
roeymarinov <roeymarinov@gmail.com>
2022-04-04 19:29:31 +0300

    profilestack

--- screens/Profile_user/profileStack.js
+++ screens/Profile_user/profileStack.js
@@ -1,21 +1,37 @@
-import { createStackNavigator } from "react-navigation-stack";
+import { createStackNavigator } from '@react-navigation/stack';
 import { createAppContainer } from "react-navigation";
 import ProfileScreen from "./User_profile";
 import EditProfileScreen from "./Edit_profile";
 import SupportScreen from "./Technical_support";
-const screens = {
-  Profile: {
-    screen: ProfileScreen,
-  },
-  Edit: {
-    screen: EditProfileScreen,
-  },
-  Thechnical: {
-    screen: SupportScreen,
-  },
-};
+// const screens = {
+//   Profile: {
+//     screen: ProfileScreen,
+//   },
+//   Edit: {
+//     screen: EditProfileScreen,
+//   },
+//   Thechnical: {
+//     screen: SupportScreen,
+//   },
+// };
 
-const ProfileStack = createStackNavigator(screens);
+// const Stack = createStackNavigator(screens);
+// const ProfileStack = () => <Stack/>
 
-//export default createAppContainer(ProfileStack);
-export default ProfileStack;
+// //export default createAppContainer(ProfileStack);
+// export default ProfileStack;
+
+const Stack = createStackNavigator();
+
+export default function ProfileStack() {
+  const options = {headerShown: false}
+
+  return (
+      <Stack.Navigator screenOptions={options}>
+          <Stack.Screen name="Profile" component={ProfileScreen} />
+          {/* <Stack.Screen name="Edit" component={EditProfileScreen} />
+          <Stack.Screen name="Thechnical" component={SupportScreen} /> */}
+
+      </Stack.Navigator>
+  );
+}
