e4e313d5a48c935efe13784d021c13321aa2dcf1
NofarVered <nofarss79@gmail.com>
2022-04-05 21:25:26 +0300

    profile changes

--- screens/Ad_dog/ad_page.js
+++ screens/Ad_dog/ad_page.js
@@ -4,29 +4,31 @@ import { Nofar_styles } from "../utils/Nofar_style";
 
 import { AR_styles } from "./Ad_style";
 import AdTemplate from "./ad_template";
-export default function AdPage({navigation}) {
+export default function AdPage({ navigation }) {
   return (
     <View style={Nofar_styles.container}>
       <ScrollView style={AR_styles.content}>
         <AdTemplate />
-        <View style={{ marginLeft: 15 }}>
-          <View style={AR_styles.cardContent}>
-            <TouchableOpacity style={Nofar_styles.BigButton} onPress={() => {}}>
-              <Text style={Nofar_styles.BigButtonText}>פתח מיקום במפה</Text>
-            </TouchableOpacity>
-          </View>
+        <View style={AR_styles.confirmBTContainer}>
+          <Button
+            mode={"contained"}
+            style={Nofar_styles.BigButton}
+            onPress={() => {}}
+          >
+            <Text style={Nofar_styles.BigButtonText}>פתח מיקום במפה</Text>
+          </Button>
+        </View>
 
-          <View style={AR_styles.cardContent}>
-            <TouchableOpacity style={Nofar_styles.BigButton} onPress={() => {navigation.replace("CreateAd")}}>
-              <Text style={Nofar_styles.BigButtonText}> עדכן את המודעה</Text>
-            </TouchableOpacity>
-          </View>
-
-          <View style={AR_styles.cardContent}>
-            <TouchableOpacity style={Nofar_styles.BigButton} onPress={() => {}}>
-              <Text style={Nofar_styles.BigButtonText}> מחק את המודעה</Text>
-            </TouchableOpacity>
-          </View>
+        <View style={AR_styles.confirmBTContainer}>
+          <Button
+            mode={"contained"}
+            style={Nofar_styles.BigButton}
+            onPress={() => {
+              navigation.replace("CreateAd");
+            }}
+          >
+            <Text style={Nofar_styles.BigButtonText}>עדכן את המודעה</Text>
+          </Button>
         </View>
       </ScrollView>
     </View>
