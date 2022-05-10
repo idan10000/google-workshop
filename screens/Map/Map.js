aaa46229c1dd4e30c287fdbba7f817576c6fab05
idan10000 <49813824+idan10000@users.noreply.github.com>
2022-05-10 22:52:03 +0300

    Milestone 2

--- screens/Map/Map.js
+++ screens/Map/Map.js
@@ -1,16 +1,9 @@
 import * as React from "react";
-import {
-  Dimensions,
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  Button,
-} from "react-native";
+import {ActivityIndicator, Dimensions, StyleSheet, Text, View} from "react-native";
 import * as Location from "expo-location";
-import MapView, { Callout, Circle, Marker } from "react-native-maps";
-import { useEffect, useState } from "react";
-import BottomSheet from "reanimated-bottom-sheet";
+import MapView, {Callout, Circle, Marker} from "react-native-maps";
+import {useEffect, useState} from "react";
+
 
 // ------------ TO DO LIST: --------------
 // 1) Add two red pins of reports + BottomSheets
@@ -18,133 +11,183 @@ import BottomSheet from "reanimated-bottom-sheet";
 // 3) Navigation from the create poster\ report
 
 export default function Map() {
-  // buttom sheet:
-  const renderContent = () => (
-    <View
-      style={{
-        backgroundColor: "lightgrey",
-        padding: 20,
-        height: 450,
-      }}
-    >
-      <Image source={{}}></Image>
-    </View>
-  );
-
-  const sheetRef = React.useRef(null);
-
-  // user live location :
-  const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
-  useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== "granted") {
-        setErrorMsg("Permission to access location was denied");
-        return;
-      }
-      console.log(status);
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-    })();
-  }, []);
-
-  let text = "Waiting..";
-  if (errorMsg) {
-    text = errorMsg;
-    console.log(text);
-  } else if (location) {
-    text = JSON.stringify(location);
-    console.log(text);
-  }
-
-  //user location
-  const [pin, setPin] = React.useState({
-    latitude: 37.78825,
-    longitude: -122.4324,
-  });
-  const [reportOne, setReportOne] = React.useState({
-    latitude: 37.78825,
-    longitude: -122.4324,
-  });
-  return (
-    <View style={{ marginTop: 50, flex: 1 }}>
-      <MapView
-        style={styles.map}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-        provider="google"
-      >
-        <Marker
-          coordinate={reportOne}
-          pinColor="blue"
-          draggable={true}
-          onDragStart={(e) => {
-            console.log("Drag start", e.nativeEvent.coordinates);
-          }}
-          onDragEnd={(e) => {
-            setReportOne({
-              latitude: e.nativeEvent.coordinate.latitude,
-              longitude: e.nativeEvent.coordinate.longitude,
-            });
-          }}
-          onPress={() => sheetRef.current.snapTo(0)}
+    const renderContent = () => (
+        <View
+            style={{
+                backgroundColor: "lightgrey",
+                padding: 20,
+                height: 450,
+            }}
         >
-          <Callout>
-            <Text>Match!</Text>
-          </Callout>
-        </Marker>
+            <Text>SHOULD ADD IMG</Text>
+        </View>
+    );
 
-        <Marker
-          coordinate={pin}
-          pinColor="red"
-          draggable={true}
-          onDragStart={(e) => {
-            console.log("Drag start", e.nativeEvent.coordinates);
-          }}
-          onDragEnd={(e) => {
+    const sheetRef = React.useRef(null);
+    // user live location :
+    const [location, setLocation] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        (async () => {
+            let {status} = await Location.requestForegroundPermissionsAsync();
+            if (status !== "granted") {
+                setErrorMsg("Permission to access location was denied");
+                return;
+            }
+            console.log(status);
+
+            console.log("before location");
+            let newLocation = await Location.getCurrentPositionAsync({});
+            console.log("after location");
+            setLocation(newLocation);
             setPin({
-              latitude: e.nativeEvent.coordinate.latitude,
-              longitude: e.nativeEvent.coordinate.longitude,
-            });
-          }}
-        >
-          <Callout>
-            <Text>I'm here</Text>
-          </Callout>
-        </Marker>
-        <Circle
-          center={pin}
-          radius={3000}
-          strokeWidth={8}
-          strokeColor={"red"}
-        />
-      </MapView>
-      <BottomSheet
-        ref={sheetRef}
-        snapPoints={["25%", "10%", "0%"]}
-        borderRadius={30}
-        initialSnap={2}
-        enabledGestureInteraction={true}
-        renderContent={renderContent}
-      />
-    </View>
-  );
+                latitude: newLocation.coords.latitude,
+                longitude: newLocation.coords.longitude,
+            })
+            setReportOne({
+                latitude: newLocation.coords.latitude,
+                longitude: newLocation.coords.longitude,
+            })
+            console.log(location);
+            setIsLoading(false)
+        })();
+    }, []);
+
+    let text = "Waiting..";
+    if (errorMsg) {
+        text = errorMsg;
+        console.log(text);
+    } else if (location) {
+        text = JSON.stringify(location);
+        console.log(text);
+    }
+
+
+    //user location
+    const [pin, setPin] = React.useState({
+        latitude: 0,
+        longitude: 0,
+    });
+
+    const [reportOne, setReportOne] = React.useState({
+        latitude: 0,
+        longitude: 0,
+    });
+
+    if (isLoading)
+        return (
+            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                <ActivityIndicator size='large'/>
+            </View>
+        )
+
+    return (
+        <View style={{marginTop: 50, flex: 1}}>
+            <MapView
+                style={styles.map}
+                initialRegion={{
+                    latitude: location.coords.latitude,
+                    longitude: location.coords.longitude,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                }}
+                provider="google"
+            >
+                <Marker
+                    coordinate={reportOne}
+                    pinColor="blue"
+                    draggable={true}
+                    onDragStart={(e) => {
+                        console.log("Drag start", e.nativeEvent.coordinates);
+                    }}
+                    onDragEnd={(e) => {
+                        setReportOne({
+                            latitude: e.nativeEvent.coordinate.latitude,
+                            longitude: e.nativeEvent.coordinate.longitude,
+                        });
+                    }}
+                    onPress={() => sheetRef.current.snapTo(0)}
+                >
+                    <Callout>
+                        <Text>Match!</Text>
+                    </Callout>
+                </Marker>
+                <Marker
+                    coordinate={pin}
+                    pinColor="red"
+                    draggable={true}
+                    onDragStart={(e) => {
+                        console.log("Drag start", e.nativeEvent.coordinates);
+                    }}
+                    onDragEnd={(e) => {
+                        setPin({
+                            latitude: e.nativeEvent.coordinate.latitude,
+                            longitude: e.nativeEvent.coordinate.longitude,
+                        });
+                    }}
+                >
+                    <Callout>
+                        <Text>I'm here</Text>
+                    </Callout>
+                </Marker>
+                <Circle
+                    center={pin}
+                    radius={3000}
+                    strokeWidth={8}
+                    strokeColor={"red"}
+                />
+            </MapView>
+
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  map: {
-    width: Dimensions.get("window").width,
-    height: Dimensions.get("window").height,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    map: {
+        width: Dimensions.get("window").width,
+        height: Dimensions.get("window").height,
+    },
 });
+
+// export default function App() {
+//   const renderContent = () => (
+//     <View
+//       style={{
+//         backgroundColor: "lightgrey",
+//         padding: 20,
+//         height: 450,
+//       }}
+//     >
+//       <Text>Swipe down to close</Text>
+//     </View>
+//   );
+
+//   const sheetRef = React.useRef(null);
+
+//   return (
+//     <>
+//       <View>
+//         <Button
+//           title="Open Bottom Sheet"
+//           onPress={() => sheetRef.current.snapTo(0)}
+//         />
+//       </View>
+//       <BottomSheet
+//         ref={sheetRef}
+//         snapPoints={["25%", "10%", "0%"]}
+//         borderRadius={30}
+//         initialSnap={2}
+//         enabledGestureInteraction={true}
+//         renderContent={renderContent}
+//       />
+//     </>
+//   );
+// }
