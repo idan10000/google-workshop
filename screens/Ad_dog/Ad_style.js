f872ac8ed38735ab273c6dd123e8513331f5889c
ArielBIn <arielbinsky@gmail.com>
2022-05-10 19:33:40 +0300

    added background and colors for all accessible page.

--- screens/Ad_dog/Ad_style.js
+++ screens/Ad_dog/Ad_style.js
@@ -63,6 +63,7 @@ export const AR_styles = StyleSheet.create({
     // marginHorizontal: "5%",
     justifyContent:"center",
     height:"6%",
+    marginVertical:"3%",
   },
   verticalLine:{
     height: 16,
