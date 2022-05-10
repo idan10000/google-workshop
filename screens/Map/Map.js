6a0937a778bc8ab556a913fb44eccaf902158100
NofarVered <nofarss79@gmail.com>
2022-05-10 19:45:39 +0300

    user location

--- screens/Map/Map.js
+++ screens/Map/Map.js
@@ -10,6 +10,29 @@ import MapView, { Callout, Circle, Marker } from "react-native-maps";
 // 3) Navigation from the create poster\ report
 
 export default function Map() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = "Waiting..";
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+  console.log(text);
+
   //user location
   const [pin, setPin] = React.useState({
     latitude: 37.78825,
