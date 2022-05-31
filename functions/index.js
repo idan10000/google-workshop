4d1e1f721beadfaf9a50178cf659fefec6a7abed
roeymarinov <roeymarinov@gmail.com>
2022-05-31 12:45:09 +0300

    sendMatchNotification

--- functions/index.js
+++ functions/index.js
@@ -149,6 +149,78 @@ exports.classifyReport = functions.firestore
         });
     });
 
+const sendMatchNotification = (uids, reports) => {
+
+    const promises = []
+    for (const uid of uids){
+        const promise = db.doc("Users/" + uid).get();
+        promises.push(promise);
+    }
+
+    return Promise.all(promises).then((snapshots) => {
+        const pushTokens = [];
+        for (const snapshot of snapshots){
+            // Each push token looks like ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]
+            const token = snapshot.get("notificationsToken");
+            pushTokens.push(token);
+        }
+
+        // Create a new Expo SDK client
+        // optionally providing an access token if you have enabled push security
+        const expo = new Expo({accessToken: process.env.EXPO_ACCESS_TOKEN});
+
+        // Create the messages that you want to send to clients
+        const messages = [];
+        for (const pushToken of pushTokens) {
+            for (const report of reports){
+                // Check that all your push tokens appear to be valid Expo push tokens
+                if (!Expo.isExpoPushToken(pushToken)) {
+                    console.error(`Push token ${pushToken} is not a valid Expo push token`);
+                    continue;
+                }
+
+                // Construct a message (see https://docs.expo.io/push-notifications/sending-notifications/)
+                messages.push({
+                    to: pushToken,
+                    sound: "default",
+                    title: "מצאנו כלב! יכול להיות שהוא שלכם?",
+                    body: "היכנסו לדיווח לפרטים נוספים",
+                    data: {report: report},
+                });
+            }
+        }
+
+        // The Expo push notification service accepts batches of notifications so
+        // that you don"t need to send 1000 requests to send 1000 notifications. We
+        // recommend you batch your notifications to reduce the number of requests
+        // and to compress them (notifications with similar content will get
+        // compressed).
+        const chunks = expo.chunkPushNotifications(messages);
+        const tickets = [];
+        (() => {
+            // Send the chunks to the Expo push notification service. There are
+            // different strategies you could use. A simple one is to send one chunk at a
+            // time, which nicely spreads the load out over time:
+            for (const chunk of chunks) {
+                try {
+                    return expo.sendPushNotificationsAsync(chunk).then((ticketChunk) => {
+                        console.log(ticketChunk);
+                        tickets.push(...ticketChunk);
+                        // NOTE: If a ticket contains an error code in ticket.details.error, you
+                        // must handle it appropriately. The error codes are listed in the Expo
+                        // documentation:
+                        // https://docs.expo.io/push-notifications/sending-notifications/#individual-errors
+                    });
+                } catch (error) {
+                    console.error(error);
+                }
+            }
+        })();
+        return tickets;
+    });
+}
+
+
 exports.sendNotifications = functions.firestore
     .document("Posters/{posterID}")
     .onCreate((snap, context) => {
